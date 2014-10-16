275c27bc1bbfa83356f9a24d8b65ed71eb41b1c0
Ashley Nolan <anolan@tmw.co.uk>
2014-10-16 11:41:38 +0100

    Updating grunt-browserify to latest version and updating grunt options to reflect

--- _grunt-configs/javascript.js
+++ _grunt-configs/javascript.js
@@ -26,7 +26,7 @@ module.exports.tasks = {
 			src: ['<%=config.js.srcFile%>'],
 			dest: '<%=config.js.distDir%><%=config.js.distFile%>',
 			options : {
-				bundleOptions : {
+				browserifyOptions : {
 					debug: true
 				}
 			}
